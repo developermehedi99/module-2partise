16b8d4edd385df159822ae756db6350ed007211c
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2024-10-14 00:26:48 +0600

    dynamic list done

--- src/components/task/Taskboard.jsx
+++ src/components/task/Taskboard.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
 import SearchTask from "./SearchTask";
 import TaskAction from "./TaskAction";
 import TaskList from "./TaskList";
 
 const Taskboard = () => {
+    const initialValue ={
+        'id':crypto.randomUUID(),
+        'title':'Learn react-next',
+        'description':'React js is a most popular library in javascript',
+        'tags':['js','react','next'],
+        'priority':'high',
+        'isFavorite':true
+    }
+    const [tasks, setTasks] = useState([initialValue]);
+
   return (
     <section className="mb-20" id="tasks">
       <div className="container">
@@ -12,7 +23,7 @@ const Taskboard = () => {
 
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <TaskAction></TaskAction>
-          <TaskList></TaskList>
+          <TaskList tasks={tasks}></TaskList>
         </div>
       </div>
     </section>
