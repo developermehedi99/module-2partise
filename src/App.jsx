7f84fd94d3be6f087febceaae55fc7733833cc56
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2024-10-13 23:36:36 +0600

    ui done

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@
 import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
+import HeroSec from './components/HeroSec'
+import Taskboard from './components/task/Taskboard'
 
 function App() {
 
   return (
     <>
       <Header></Header>
+      <div className="flex flex-col justify-center items-center">
+      <HeroSec></HeroSec>
+      <Taskboard></Taskboard>
+      </div>
       <Footer></Footer>
     </>
   )
